7f4a912c2034a709e65665af7495afb2e98891ae
Arturo-Blanco <arturoblanco@tdf.edu.ar>
2023-12-02 20:50:54 -0300

    features are added

--- src/auth/services/authServices.mjs
+++ src/auth/services/authServices.mjs
@@ -5,7 +5,13 @@ export const loginServices = async (userData) => {
     const response = await axios.post(`${apiPath}auth/login`, userData);
     return response.data;
 }
-export const registerServices = async(userData) => {
+
+export const registerServices = async (userData) => {
     const response = await axios.post(`${apiPath}auth/register`, userData);
+    return response.data;
+}
+
+export const restorePass = async(userData) => {
+    const response = await axios.post(`${apiPath}user/restore/password` , userData)
     return response.data;
 }
