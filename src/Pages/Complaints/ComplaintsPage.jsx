b95caec546cf6b587664c4582a03ccb4d32fa0ac
= <dariocabrera@tdf.edu.ar>
2023-12-03 21:53:34 -0300

    card funcional

--- src/Pages/Complaints/ComplaintsPage.jsx
+++ src/Pages/Complaints/ComplaintsPage.jsx
@@ -6,7 +6,8 @@ import { useEffect, useState } from 'react';
 import './complaintsPage.css';
 
 const ComplaintsPage = () => {
-
+    
+    const complaintType = ["Mascota perdida", "Mascota encontrada", "Maltrato", "Otro"];
     const [complainsList, setComplaintList] = useState([]);
 
     useEffect(() => {
@@ -36,9 +37,9 @@ const ComplaintsPage = () => {
                     {complainsList.map((complaint, index) => (
                         <InformationCard
                             key={index}
-                            title = {complaint.typeOfComplaint}
-                            imageUrl={complaint.file}
-                            body = {complaint.complaintDescription}
+                            title = {complaintType[complaint.fk_complaint_type_id]}
+                            imageUrl={complaint.img_url}
+                            body = {complaint.description}
                         />
                     ))}
                 </section>
@@ -48,9 +49,3 @@ const ComplaintsPage = () => {
 }
 export default ComplaintsPage;
 
-const complaints = [
-    { urlImg: 'https://www.elespectador.com/resizer/X0hDCeSGTTKyJlD-YOFtPczYaSI=/631x420/filters:quality(60):format(jpeg)/cloudfront-us-east-1.images.arcpublishing.com/elespectador/WFG2DWO6ZNGMJNXHVT4SCNII2Y.jpeg', title: 'No al maltrato', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id ornare arcu odio ut sem. Vitae tempus quam pellentesque nec. Elit ut aliquam purus sit amet luctus venenatis lectus. Enim ut tellus elementum sagittis vitae et leo duis. Massa eget egestas purus viverra accumsan in nisl nisi. A diam sollicitudin tempor id. Aliquam sem fringilla ut morbi tincidunt augue interdum. Nibh praesent tristique magna sit amet purus gravida quis. Eu sem integer vitae justo eget magna fermentum iaculis. Vel quam elementum pulvinar etiam non quam. Sed velit dignissim sodales ut eu. Vitae sapien pellentesque habitant morbi tristique senectus. Cursus metus aliquam eleifend mi in nulla. Cras tincidunt lobortis feugiat vivamus at augue eget. Orci sagittis eu volutpat odio facilisis. Nec sagittis aliquam malesuada bibendum.' },
-    { urlImg: 'https://notasdemascotas.com/wp-content/uploads/2020/05/70-perros-rescatados-de-una-granja.jpg', title: 'No al maltrato', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id ornare arcu odio ut sem. Vitae tempus quam pellentesque nec. Elit ut aliquam purus sit amet luctus venenatis lectus. Enim ut tellus elementum sagittis vitae et leo duis. Massa eget egestas purus viverra accumsan in nisl nisi. A diam sollicitudin tempor id. Aliquam sem fringilla ut morbi tincidunt augue interdum. Nibh praesent tristique magna sit amet purus gravida quis. Eu sem integer vitae justo eget magna fermentum iaculis. Vel quam elementum pulvinar etiam non quam. Sed velit dignissim sodales ut eu. Vitae sapien pellentesque habitant morbi tristique senectus. Cursus metus aliquam eleifend mi in nulla. Cras tincidunt lobortis feugiat vivamus at augue eget. Orci sagittis eu volutpat odio facilisis. Nec sagittis aliquam malesuada bibendum.' },
-    { urlImg: 'https://proyecto13.com/up-p13/uploads/2020/04/WhatsApp-Image-2020-04-21-at-12.43.54-2.jpeg', title: 'No al maltrato', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id ornare arcu odio ut sem. Vitae tempus quam pellentesque nec. Elit ut aliquam purus sit amet luctus venenatis lectus. Enim ut tellus elementum sagittis vitae et leo duis. Massa eget egestas purus viverra accumsan in nisl nisi. A diam sollicitudin tempor id. Aliquam sem fringilla ut morbi tincidunt augue interdum. Nibh praesent tristique magna sit amet purus gravida quis. Eu sem integer vitae justo eget magna fermentum iaculis. Vel quam elementum pulvinar etiam non quam. Sed velit dignissim sodales ut eu. Vitae sapien pellentesque habitant morbi tristique senectus. Cursus metus aliquam eleifend mi in nulla. Cras tincidunt lobortis feugiat vivamus at augue eget. Orci sagittis eu volutpat odio facilisis. Nec sagittis aliquam malesuada bibendum.' },
-    { urlImg: 'https://images.hola.com/imagenes/estar-bien/20190115135374/california-ley-venta-mascotas-gt/0-634-831/perros-ley-california-t.jpg?tx=w_680', title: 'No al maltrato', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id ornare arcu odio ut sem. Vitae tempus quam pellentesque nec. Elit ut aliquam purus sit amet luctus venenatis lectus. Enim ut tellus elementum sagittis vitae et leo duis. Massa eget egestas purus viverra accumsan in nisl nisi. A diam sollicitudin tempor id. Aliquam sem fringilla ut morbi tincidunt augue interdum. Nibh praesent tristique magna sit amet purus gravida quis. Eu sem integer vitae justo eget magna fermentum iaculis. Vel quam elementum pulvinar etiam non quam. Sed velit dignissim sodales ut eu. Vitae sapien pellentesque habitant morbi tristique senectus. Cursus metus aliquam eleifend mi in nulla. Cras tincidunt lobortis feugiat vivamus at augue eget. Orci sagittis eu volutpat odio facilisis. Nec sagittis aliquam malesuada bibendum.' }
-]
