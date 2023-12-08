f55a1223b0625b703d996d9d7743d653a0a03294
= <dariocabrera@tdf.edu.ar>
2023-12-08 13:13:08 -0300

    server

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: {
+        host: "localhost",
+        protocol: "ws",
+    }
+  },
   plugins: [react()],
 })
